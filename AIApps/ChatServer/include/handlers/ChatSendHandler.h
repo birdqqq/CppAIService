427a65084ef57784447746ae290f83a6a71b45f0
birdqqq <2133957335@qq.com>
2026-07-17 18:40:13 +0800

    some comments

--- AIApps/ChatServer/include/handlers/ChatSendHandler.h
+++ AIApps/ChatServer/include/handlers/ChatSendHandler.h
@@ -4,6 +4,7 @@
 #include "../ChatServer.h"
 
 class ChatSendHandler : public http::router::RouterHandler
+// 策略模式 根据url的路径，通过路由表调用对应的handler 而不是冗余的 if()  else if()   else if() ... 
 {
 public:
     explicit ChatSendHandler(ChatServer* server) : server_(server) {}
